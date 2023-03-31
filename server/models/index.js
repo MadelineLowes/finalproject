2f53f5ee5c4f8aef27f7ee7d7f6220f6a5e31708
Robert Alexander Iatan <robertiatan@gmail.com>
2023-03-31 15:19:23 -0400

    refactor

--- server/models/index.js
+++
@@ -1,5 +0,0 @@
-const User = require("./User");
-const Category = require("./Category");
-const Cause = require("./Cause");
-
-module.exports = { User, Category, Cause };
--- server/schemas/index.js
+++
@@ -1,5 +0,0 @@
-// // from activity 21.28
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
-
-module.exports = { typeDefs, resolvers };
